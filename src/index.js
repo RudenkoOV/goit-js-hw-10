a3af935bb5649176c23cd42fc9129cef6e45f020
RudenkoOV <kpaxx@ukr.net>
2023-02-06 21:53:29 +0200

    3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const DEBOUNCE_DELAY = 3000;
 
-const DEBOUNCE_DELAY = 300;
+const searchForm = document.getElementById('search-box');
+console.log(searchForm);
+searchForm.addEventListener('input', debounce((event) => {
+    event.preventDefault();
+    const form = event.target.value;
+    console.log(form);
+}, DEBOUNCE_DELAY));
+    
+
+
+
+function fetchCountries(name) {
+  fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => response.json()).then(console.log);
+}
+
+const USA = fetchCountries("usa");
+// console.log(USA);
+
+// 
+// function feachCountryByName(name) {
+//     console.log(fetch(`https://restcountries.com/v3.1/name/${name}`));
+//         // .then(console.log).catch(Notify.failure(`❌ Oops, there is no country with that name`)).finally()
+// }
+
+
