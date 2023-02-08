dd49a6e376520bb7d29996b709b7de875dca9a4d
RudenkoOV <kpaxx@ukr.net>
2023-02-08 18:56:20 +0200

    7

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './js/fetchCountries';
+import { makeMarkup } from './js/makeMarkup';
+
 const DEBOUNCE_DELAY = 300;
 const searchForm = document.getElementById('search-box');
 const countryListEl = document.querySelector('.country-list');
@@ -9,34 +11,6 @@ const countryInfoEl = document.querySelector('.country-info');
 searchForm.addEventListener('input', debounce((event) => {
     event.preventDefault();
     const form = event.target.value.trim();
-    fetchCountries(form)
-}, DEBOUNCE_DELAY)); 
+    fetchCountries(form).then(country => makeMarkup(country,countryListEl,countryInfoEl), DEBOUNCE_DELAY).catch(error => console.log(error))
+}));
 
-function fetchCountries(name) {
-    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,flags,population,languages`).then(response => response.json()).then(country => {
-        let markUp = [];
-        let markUpDiv = [];
-        
-        if (country.length < 20 & country.length > 0) {    
-            if (country.length == 1) {
-                for (each of country) {
-            markUp.push(`<li class="country__list"><img class="country__list__image" src="${each.flags.svg}" alt="${each.name.official}" width="30"></img><p style = "font-size: 40px">${each.name.official}</p></li>`);
-            markUpDiv.push(`
-            <ul class="country__info">
-            <li class="country__info__item">Capital: <span class="country__info__span">${each.capital}</span></li>
-            <li  class="country__info__item">Population: <span class="country__info__span">${each.population}</span></li>
-            <li class="country__info__item">Languages: <span class="country__info__span">${Object.values(Object.values(each.languages))}</span></li>
-            </ul>`);
-            }}
-            else { 
-                for (each of country) {
-                markUp.push(`<li class="country__list"><img class="country__list__image" src="${each.flags.svg}" alt="${each.name.official}" width="40"></img><p>${each.name.official}</p> </li>`);
-                }
-        } 
-        }    else if (!country.length) {return} else { Notify.info('Too many matches found. Please enter a more specific name.')};
-        countryListEl.innerHTML = markUp.join('');
-        countryInfoEl.innerHTML = markUpDiv;
-  }).catch(error => console.log(error));
-}
-
-// Notify.failure('Oops, there is no country with that name')
